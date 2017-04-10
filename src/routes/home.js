f45bb590ed0ee7a0549fcbc8f61b531cc0cc36aa
Zooey <zooey@lbk.org.uk>
2017-04-10 15:28:56 +0100

    redirect to github We have made it so that our/login path redirects to github authorization relates #6

---
+++ src/routes/home.js
@@ -0,0 +1,7 @@
+module.exports = {
+  method: "GET",
+  path: "/",
+  handler: {
+    file: __dirname + "/../../public/index.html",
+  }
+};
--- src/server.js
+++ src/server.js
@@ -1,27 +1,24 @@
 const hapi = require("hapi");
 const inert = require("inert");
-const fs = require('fs');
+const login = require("./routes/login.js");
+const home = require("./routes/home.js");
+const fs = require("fs");
 
+
+console.log(home, login);
 const server = new hapi.Server();
 
 server.connection({
   port: process.env.PORT || 4000,
   tls: {
-    key: fs.readFileSync(__dirname + '/../keys/key.pem'),
-    cert: fs.readFileSync(__dirname + '/../keys/cert.pem')
-    }
+    key: fs.readFileSync(__dirname + "/../keys/key.pem"),
+    cert: fs.readFileSync(__dirname + "/../keys/cert.pem")
+  }
 });
 
 server.register(inert, (err) => {
   if (err) throw err;
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: {
-      file: __dirname + "/../public/index.html",
-    }
-  });
-  // console.log(server.info);
+  server.route([home, login]);
 });
 
 server.start((err) => {
