a74b9c4a9120c4519d05a74fd416d9364310d4f3
bo-bok <bo-bok@users.noreply.github.com>
2017-04-10 18:07:03 +0100

    reply with home view after user has authenticated
    relates #9

--- src/routes/welcome.js
+++ src/routes/welcome.js
@@ -8,21 +8,15 @@ module.exports = {
     request.post(`https://github.com/login/oauth/access_token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${req.url.query.code}`,
       function (error, response, body) {
         console.log(body);
+        reply.redirect('/');
     });
 
   // console.log(process.env.CLIENT_ID, process.env.CLIENT_SECRET);
   // reply(req.url.query.code);
 
-
   // config file (require it in)
   // request.post method
   // having the correct query string
-  //
-  //
-
-  // where to place:  
-  // reply.redirect('./home');
-
 
   }
 }
